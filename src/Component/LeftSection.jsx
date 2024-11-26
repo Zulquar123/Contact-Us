1b864547a991182ad68cb9e8de05b264887c3ba7
code-with-zulquar <codewithzulquar@gmail.com>
2024-11-26 23:14:04 +0530

    update

--- src/Component/LeftSection.jsx
+++ src/Component/LeftSection.jsx
@@ -3,7 +3,7 @@ import Form from "./Form";
 
 export default function LeftSection() {
   return (
-    <div className="mt-9">
+    <div className="mt-9 px-4 sm:px-6 lg:px-8">
       <FormHeader />
       <Form />
     </div>
